0acee1e24f38ad48bf570f0525631735d55959f4
tawsifhye <tawsif.lucse@gmail.com>
2021-10-10 01:07:32 +0600

    Firebase hook for shared authentication

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDkmwWD56aZ-3omXYgqWyoTXFANG0XARZE",
+    authDomain: "react-auth-integration-8e77e.firebaseapp.com",
+    projectId: "react-auth-integration-8e77e",
+    storageBucket: "react-auth-integration-8e77e.appspot.com",
+    messagingSenderId: "766777347410",
+    appId: "1:766777347410:web:7aec9c605442670d507e8b"
+  };
+
+  export default firebaseConfig;
---
+++ src/hook/userFirebase.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import initializeAuthentication from '../Firebase/firebase.init'
+
+
+initializeAuthentication()
+
+const useFirebase = () =>{
+    const [user, setUser] = useState({});
+    const [error, setError] = useState('');
+    const auth = getAuth();
+    const googleProvide = new GoogleAuthProvider();
+    const signInUsingGoogle = () => {
+        signInWithPopup(auth, googleProvide)
+        .then(result =>{
+            console.log(result.user);
+            setUser(result.user)
+        })
+        .catch(error =>{
+            setError(error.messgae)
+        })
+    }
+
+    return{
+        signInUsingGoogle
+    }
+}
+
+export default useFirebase;
